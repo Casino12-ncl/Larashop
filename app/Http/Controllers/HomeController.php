acd8ce930a421473bb4e2dace512a643cf9f2129
casino12 <istomin_84@mail.ru>
2023-03-21 17:16:05 +0400

    add model for Products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+        $products = Product::all();
+        foreach($products as $product) {
+            echo 'Title ' .$product->title;
+            echo 'Price ' .$product->price;
+            echo '<br>';
+            echo '---------';
+            echo '<br>';
+
+        }
+        //return view('home.index');
     }
 }
