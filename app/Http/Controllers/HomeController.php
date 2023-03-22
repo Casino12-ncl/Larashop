7a51896859faae25e695f53f48ff49a35eb183fd
casino12 <istomin_84@mail.ru>
2023-03-22 18:59:59 +0400

    add products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,15 +7,10 @@ use App\Models\Product;
 class HomeController extends Controller
 {
     public function index(){
-        $products = Product::all();
-        // foreach($products as $product) {
-        //     echo 'Title ' .$product->title;
-        //     echo 'Price ' .$product->price;
-        //     echo '<br>';
-        //     echo '---------';
-        //     echo '<br>';
-
-        // }
-        return view('home.index');
+        $products = Product::orderBy('created_at')->take(8)->get();
+        
+        return view('home.index', [
+            'products' => $products
+        ]);
     }
 }
