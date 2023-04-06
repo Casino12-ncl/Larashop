d1745724e6fc431c2ad5f6196a099d14607f51ad
casino12 <istomin_84@mail.ru>
2023-04-06 16:41:02 +0400

    add login-register

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +16,12 @@ use App\Http\Controllers\CartController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/category/{cat}', [ProductController::class, 'showCategory'])->name('showCategory');
 Route::get('/category/{cat}/{product_id}', [ProductController::class, 'show'])->name('showProduct');
 Route::get('/cart', [CartController::class, 'index'])->name('cartIndex');
 Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('addToCart');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
